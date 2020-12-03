2e735920ec7e3782d5a2e89a75bd76fa78e4d80f
Liz <lizlarkin@highlandtechnology.com>
2020-12-02 20:54:42 -0800

    added conditional to set license shield based on which license is selected by user

--- index.js
+++ index.js
@@ -61,12 +61,21 @@ inquirer
         console.log(response.usernameInput);
         console.log(response.emailInput);
 
+let shield = ""
+if (response.licenseInput == "MIT License") {
+    shield = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+} else if (response.licenseInput == 'Apache License') {
+    shield = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+} else {
+    shield = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+}
+
+console.log(shield);
+
 const markdown = 
 `
 # ${response.titleInput}
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+${shield}
 ## Table of Contents:
 - [Description](###Description)
 - [Installation](###Installation)
--- readme.md
+++ readme.md
@@ -1,7 +1,5 @@
 
 # 1
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 ## Table of Contents:
 - [Description](###Description)
@@ -23,7 +21,7 @@
 ### Tests: 
 6
 ### License: 
-Apache License
+GPL License
 ### Questions:
 7
 * [7 GitHub Link](http://github.com/7)
